efbbf1fabedaa7e86ad4d4806842c9b4525abed0
Hugo.M <151736047+MICHEL-Hugo@users.noreply.github.com>
2025-01-07 11:18:32 +0100

    Add logs API

--- decisionMS/src/main/java/fr/insa/iotapp/decisionMS/controller/DecisionController.java
+++ decisionMS/src/main/java/fr/insa/iotapp/decisionMS/controller/DecisionController.java
@@ -4,6 +4,12 @@ import fr.insa.iotapp.decisionMS.service.DecisionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/decision")
 public class DecisionController {
@@ -26,5 +32,19 @@ public class DecisionController {
         decisionService.stopMonitoring();
         return "Surveillance arrêtée.";
     }
-}
 
+    @GetMapping("/logs")
+    public String getLogs() {
+        File logFile = new File("src/main/resources/logs.txt");
+
+        if (!logFile.exists()) {
+            return "Le fichier de logs est introuvable.";
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(logFile))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            return "Erreur lors de la lecture des logs : " + e.getMessage();
+        }
+    }
+}
